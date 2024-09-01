73223a678d59703ebb122d981c6507c20eb0a56e
Vijay Krishnan V <vijaykrishnan90@yahoo.com>
2024-09-01 15:28:39 +0530

    constant value is included

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+
+"""
+Defining common constant variable for training pipeline.
+"""
+TARGET_COLUMN = "Result"
+PIPELINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "NetworkData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+PREPROCESSING_OBJECT_NAME = "preprocessing.pkl"
+MODEL_FILE_NAME="model.pkl"
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+SCHEMA_DROP_COLS="drop_columns"
+SAVED_MODEL_DIR = os.path.join("save_models")
+
+"""
+Data ingestion related constant starts with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str = "network_security"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+"""
+Data validation related constant starts with DATA_VALIDATION VAR NAME
+"""
+
+"""
+Data transformation related constant starts with DATA_TRANSFORMATION VAR NAME
+"""
+
+"""
+model trainer related constant starts with MODEL_TRAINER VAR NAME
+"""
