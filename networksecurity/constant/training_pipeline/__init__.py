3e802ed356d4a94fe854921e3ad0abb8441f2c49
Vijay Krishnan V <vijaykrishnan90@yahoo.com>
2024-09-02 08:36:58 +0530

    data validation entity

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -34,7 +34,11 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 """
 Data validation related constant starts with DATA_VALIDATION VAR NAME
 """
-
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "drift_report.yaml"
 """
 Data transformation related constant starts with DATA_TRANSFORMATION VAR NAME
 """
