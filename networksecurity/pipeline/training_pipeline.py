04957edd0e07611e6d95b638ad5ca6c4ec881161
Vijay Krishnan V <vijaykrishnan90@yahoo.com>
2024-09-06 06:55:17 +0530

    updated validation and logs to be ignored

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -49,7 +49,7 @@ class TrainingPipeline:
     def start_data_validation(self, data_ingestion_artifact:DataIngestionArtifact):
         try:
             data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
-            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact, data_validation_config=data_validation_config)
             data_validation_artifact=data_validation.initiate_data_validation()
             return data_validation_artifact
         except Exception as e:
@@ -85,6 +85,6 @@ class TrainingPipeline:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             print(data_ingestion_artifact)
-            data_validation = self.start_data_validation()
+            data_validation = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
