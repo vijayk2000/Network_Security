0fe0b288fae7b68a959f3ca0cfbeb502905f26ad
Vijay Krishnan V <vijaykrishnan90@yahoo.com>
2024-09-07 16:15:04 +0530

    completed data validation

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -19,15 +19,16 @@ class DataValidation:
             raise NetworkSecurityException(e, sys)
         
 
-    def validate_no_of_column(self, dataframe:pd.DataFrame)->bool:
+    def validate_number_of_columns(self, dataframe:pd.DataFrame)->bool:
         try:
-            number_of_columns = len(self.schema_config["columns"])
+            number_of_columns = len(self._schema_config["columns"])
             logging.info(f"Required no of columns:{number_of_columns}")
             logging.info(f"Dataframe has columns: {len(dataframe.columns)}")
 
-            if len(dataframe.columns)!=number_of_columns:
+            if len(dataframe.columns)==number_of_columns:
                 return True
             return False
+        
         except Exception as e:
             raise NetworkSecurityException(e, sys)
                 
@@ -66,13 +67,13 @@ class DataValidation:
                 d1 = base_df[col]
                 d2 = current_df[col]
                 is_same_dist = ks_2samp(d1,d2)
-                if threshold <= is_same_dist.pvalue():
+                if threshold <= is_same_dist.pvalue:
                     is_found = False
                 else:
                     is_found = True
                     status = False
                 report.update({col: {
-                    "pvalue": float(is_same_dist.pvalue),
+                    "p_value": float(is_same_dist.pvalue),
                     "drift_status": is_found
                 }})
 
@@ -89,8 +90,49 @@ class DataValidation:
         
     def initiate_data_validation(self)->DataValidationArtifact:
         try:
-            self.read_data()
-            self.validate_no_of_column()
-            self.detect_dataset_drift()
+            train_file_path = self.data_ingestion_artifact.trained_file_path
+            test_file_path = self.data_ingestion_artifact.test_file_path
+
+            #Reading data from train and test file location
+            train_dataframe = DataValidation.read_data(train_file_path)
+            test_dataframe = DataValidation.read_data(test_file_path)
+            
+            #Validate number of columns
+            status = self.validate_number_of_columns(dataframe=train_dataframe)
+            if not status:
+                error_message=f"{error_message}Train dataframe does not contain all columns.\n"
+            status = self.validate_number_of_columns(dataframe=test_dataframe)
+            if not status:
+                error_message=f"{error_message}Test dataframe does not contain all columns.\n"    
+            #if len(error_message)>0:
+                #raise Exception(error_message)
+            
+            #Let check data drift
+            status = self.detect_dataset_drift(base_df=train_dataframe,current_df=test_dataframe)
+            
+            dir_path = os.path.dirname(self.data_validation_config.valid_train_file_path)
+            
+            os.makedirs(dir_path, exist_ok=True)
+            
+            train_dataframe.to_csv(
+                self.data_validation_config.valid_train_file_path, index=False, header=True
+            )
+
+            test_dataframe.to_csv(
+                self.data_validation_config.valid_test_file_path, index=False, header=True
+            )
+
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path,
+            )
+
+            logging.info(f"Data validation artifact: {data_validation_artifact}")
+
+            return data_validation_artifact
         except Exception as e:
-            raise NetworkSecurityException(e, sys)
+            raise NetworkSecurityException(e,sys)
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -86,5 +86,6 @@ class TrainingPipeline:
             data_ingestion_artifact = self.start_data_ingestion()
             print(data_ingestion_artifact)
             data_validation = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            print(data_validation)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
