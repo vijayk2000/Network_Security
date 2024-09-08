7cd0171ae2936508e97aa848a5a7b69d3c18a7e5
Vijay Krishnan V <vijaykrishnan90@yahoo.com>
2024-09-08 19:52:15 +0530

    added evaluation pipeline

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -113,6 +113,11 @@ class TrainingPipeline:
             print(data_transformation_artifact)
             
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_eval_artifact=self.start_model_evaluation(data_validation_artifact=data_validation_artifact,model_trainer_artifact=model_trainer_artifact)
+            if not model_eval_artifact.is_model_accepted:
+                #raise Exception("Trained model is not better than the best model")
+                print("Trained model is not better than the best model")
+            print(model_eval_artifact)
 
 
     
