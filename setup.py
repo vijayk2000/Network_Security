24ed0f32b072ed39c89509b76b0905fb9e56f5d5
Vijay Krishnan V <vijaykrishnan90@yahoo.com>
2024-12-18 15:41:43 +0530

    updated set up

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ print(get_requirements())
 setup(
     name="networksecurity",
     version="0.0.1",
-    author="sunny savita",
-    author_email="snshrivas3365@gmail.com",
+    author="vijay krishnan",
+    author_email="test@test.com",
     packages = find_packages(),
     install_requires=get_requirements()
 )
